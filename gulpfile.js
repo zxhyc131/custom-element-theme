1137b65ec4dc1bb71ec27b26b4a89c8f096b6588
侯怡辰 <zxhyc131@qq.com>
2022-10-13 11:48:41 +0800

    init

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,22 @@ var path = require('path')
 var gulp = require('gulp')
 var cleanCSS = require('gulp-clean-css');
 var cssWrap = require('gulp-css-wrap');
+var themeName = 'blue'
+var customThemeName = `${themeName}-theme`
+var customThemeClassName = `.${customThemeName}`
 
-var customThemeName='.custom-theme'
-
-gulp.task('css-wrap', function() {
-  return gulp.src( path.resolve('./theme/index.css'))
-    .pipe(cssWrap({selector:customThemeName}))
+gulp.task('css-wrap', function () {
+  // 移动config.json 到自定义主题目录
+  gulp.src(path.resolve(`./${themeName}/config.json`))
+    .pipe(gulp.dest(customThemeName))
+  return gulp.src(path.resolve(`./${themeName}/theme/index.css`))
+    .pipe(cssWrap({ selector: customThemeClassName }))
     .pipe(cleanCSS())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(customThemeName))
 });
 
-gulp.task('move-font', function() {
-  return gulp.src(['./theme/fonts/**']).pipe(gulp.dest('dist/fonts'));
+gulp.task('move-font', function () {
+  return gulp.src([`./${themeName}/theme/fonts/**`]).pipe(gulp.dest(`${customThemeName}/fonts`));
 });
 
-gulp.task('default',['css-wrap','move-font']);
+gulp.task('default', ['css-wrap', 'move-font']);
